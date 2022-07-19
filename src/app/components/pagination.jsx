72da4f29c2724e5acf6434c88e6a908d4a65c6c5
Gregdabro <gregmonak@gmail.com>
2022-07-19 13:05:24 +0200

    add state for currentPage and set state handlePageChange

--- src/app/components/pagination.jsx
+++ src/app/components/pagination.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 
-const Pagination = (props) => {
-    const { itemsCount, pageSize } = props;
+const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
     const pageCount = Math.ceil(itemsCount / pageSize);
     if ( pageCount === 1 ) return null;
     const pages = _.range(1, pageCount + 1);
@@ -10,7 +9,15 @@ const Pagination = (props) => {
         <nav>
             <ul className="pagination">
                 {pages.map(page => (
-                    <li className="page-item" key={'page_'+page}><button className="page-link">{page}</button></li>
+                    <li
+                        className={"page-item" + (page === currentPage ? " active" : "")}
+                        key={'page_'+page}>
+                        <button
+                            className="page-link"
+                            onClick={() => onPageChange(page)}>
+                            {page}
+                        </button>
+                    </li>
                 ))}
             </ul>
         </nav>
